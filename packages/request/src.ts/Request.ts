e24a31d0c2426505cf991bc5d77be10b80b9aa39
oobiora <oo608@nyu.edu>
2022-08-01 15:02:21 -0400

    +event listener import

--- packages/request/src.ts/Request.ts
+++ packages/request/src.ts/Request.ts
@@ -2,6 +2,7 @@ import { ZeroP2P } from '@zerodao/p2p';
 import peerId = require('peer-id');
 import lp from 'it-length-prefixed';
 import pipe from 'it-pipe';
+import { EventEmitter } from "events";
 
 function defer() {
   let resolve, reject, promise;
@@ -28,41 +29,41 @@ export class PublishEventEmitter extends EventEmitter {
     return deferred.promise;
   }
 }
-    
+
 
 export abstract class Request {
-    static get PROTOCOL() { throw ner Error('static get PROTOCOL() must be implemented'); }
-    public contractAddress?: string;
-    serialize(): Buffer {
-        throw new Error("Serialize must be implemented")
-    };
+  static get PROTOCOL() { throw new Error('static get PROTOCOL() must be implemented'); }
+  public contractAddress?: string;
+  serialize(): Buffer {
+    throw new Error("Serialize must be implemented")
+  };
 
-    getChainId(): string {
-        this.contractAddress
-        return
+  getChainId(): string {
+    this.contractAddress
+    return
+  }
+
+  async publish(peer: ZeroP2P): Promise<PublishEventEmitter> {
+    const request = this.serialize();
+    const result = new PublishEventEmitter();
+    if (peer.keepers.length === 0) {
+      setImmediate(() => result.emit('error', new Error('Cannot publish request if no keepers are found')));
     }
-
-    async publish(peer: ZeroP2P): PublishEventEmitter {
-        const request = this.serialize();
-	const result = new PublishEventEmitter();
-        if (peer.keepers.length === 0) {
-            setImmediate(() => result.emit('error', new Error('Cannot publish request if no keepers are found')));
+    (async () => {
+      for (const keeper of peer.keepers) {
+        try {
+          const _peerId = await peerId.createFromB58String(keeper);
+          const { stream } = await peer.dialProtocol(_peerId, this.constructor().PROTOCOL);
+          pipe(request, lp.encode(), stream.sink);
+          result.emit('dialed', keeper);
+        } catch (e: Error) {
+          result.emit('error', new Error(`Failed dialing keeper: ${keeper}`);
         }
-	(async () => {
-          for (const keeper of peer.keepers) {
-            try {
-              const _peerId = await peerId.createFromB58String(keeper);
-              const { stream } = await peer.dialProtocol(_peerId, this.constructor().PROTOCOL);
-              pipe(request, lp.encode(), stream.sink);
-              result.emit('dialed', keeper);
-            } catch (e: Error) {
-              result.emit('error', new Error(`Failed dialing keeper: ${keeper}`);
-            }
-	  }
-	  result.emit('finish');
-        })().catch((err) => result.emit('error', err));
-	return result;
-    }
+      }
+      result.emit('finish');
+    })().catch((err) => result.emit('error', err));
+    return result;
+  }
 
 
 }
