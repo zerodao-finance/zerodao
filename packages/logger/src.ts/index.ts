63c3d42232c3a4f14c408e9e253fee1fa063564f
Nicholas Khami <nicholas.khami@gmail.com>
2022-08-05 09:43:48 -0500

    chore: add logging

--- packages/logger/src.ts/index.ts
+++ packages/logger/src.ts/index.ts
@@ -2,20 +2,53 @@ import {
   createLogger as createWinstonLogger,
   transports,
   Logger,
+  format,
+  addColors
 } from "winston";
 import { UserTypes } from "./types";
 
+const customLevels = {
+  error: 0,
+  warn: 1,
+  data: 2,
+  info: 3,
+  debug: 4,
+  verbose: 5,
+  silly: 6,
+  custom: 7,
+};
+
+const customColors = {
+  error: 'red',
+  warn: 'yellow',
+  data: 'grey',
+  info: 'green',
+  debug: 'red',
+  verbose: 'cyan',
+  silly: 'magenta',
+  custom: 'blue',
+}
+
 const createLogger = (userType?: UserTypes) => {
-  createWinstonLogger({
-    level:
-      process?.env.NODE_ENV === "test" || process?.env.REACT_APP_TEST
-        ? "debug"
-        : "info",
+  addColors(customColors);
+  const logger = createWinstonLogger({
     defaultMeta: {
       service: userType ?? "zero.user",
     },
-    transports: [new transports.Console()],
+    levels: customLevels,
+    transports: [new transports.Console({
+      level: 'custom',
+      format: format.combine(
+        format.colorize({
+          all:true,
+        }),
+        format.splat(),
+        format.simple(),
+      )
+      })],
   });
+
+  return logger;
 };
 
 export default createLogger;
