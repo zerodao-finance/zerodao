e61acce8347b1570970954e44b5bae3665665a1d
altinak <altinak@zero.gg>
2022-08-16 11:46:31 -0500

    bundle SDK

--- packages/sdk/lib/index.d.ts
+++ packages/sdk/lib/index.d.ts
@@ -1,9 +1,16 @@
-export * from "@zerodao/common";
+export { FIXTURES } from "@zerodao/common";
 export * from "@zerodao/request";
-export * from "@zerodao/chains";
-export * from "@zerodao/compute";
+export { RENVM_PROVIDERS, RPC_ENDPOINTS, CONTROLLER_DEPLOYMENTS, URLS, CHAINS, NAME_CHAIN, ID_CHAIN } from "@zerodao/chains";
+export { makeQuoter, makeCompute } from "@zerodao/compute";
+import { applyRatio, computeRandomValue, getNonce, getPNonce } from "@zerodao/compute";
 export * from "@zerodao/logger";
-export * from "@zerodao/p2p";
-import * as utils from "@zerodao/utils";
-export { utils };
+export { ZeroP2P } from "@zerodao/p2p";
+import * as utilsModule from "@zerodao/utils";
 export declare const DEPLOYMENTS: any;
+declare const utils: {
+    applyRatio: typeof applyRatio;
+    computeRandomValue: typeof computeRandomValue;
+    getNonce: typeof getNonce;
+    getPNonce: typeof getPNonce;
+} & typeof utilsModule;
+export { utils };
--- packages/sdk/lib/index.js
+++ packages/sdk/lib/index.js
@@ -26,15 +26,33 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEPLOYMENTS = exports.utils = void 0;
-__exportStar(require("@zerodao/common"), exports);
+exports.utils = exports.DEPLOYMENTS = exports.ZeroP2P = exports.makeCompute = exports.makeQuoter = exports.ID_CHAIN = exports.NAME_CHAIN = exports.CHAINS = exports.URLS = exports.CONTROLLER_DEPLOYMENTS = exports.RPC_ENDPOINTS = exports.RENVM_PROVIDERS = exports.FIXTURES = void 0;
+var common_1 = require("@zerodao/common");
+Object.defineProperty(exports, "FIXTURES", { enumerable: true, get: function () { return common_1.FIXTURES; } });
 __exportStar(require("@zerodao/request"), exports);
-__exportStar(require("@zerodao/chains"), exports);
-__exportStar(require("@zerodao/compute"), exports);
+var chains_1 = require("@zerodao/chains");
+Object.defineProperty(exports, "RENVM_PROVIDERS", { enumerable: true, get: function () { return chains_1.RENVM_PROVIDERS; } });
+Object.defineProperty(exports, "RPC_ENDPOINTS", { enumerable: true, get: function () { return chains_1.RPC_ENDPOINTS; } });
+Object.defineProperty(exports, "CONTROLLER_DEPLOYMENTS", { enumerable: true, get: function () { return chains_1.CONTROLLER_DEPLOYMENTS; } });
+Object.defineProperty(exports, "URLS", { enumerable: true, get: function () { return chains_1.URLS; } });
+Object.defineProperty(exports, "CHAINS", { enumerable: true, get: function () { return chains_1.CHAINS; } });
+Object.defineProperty(exports, "NAME_CHAIN", { enumerable: true, get: function () { return chains_1.NAME_CHAIN; } });
+Object.defineProperty(exports, "ID_CHAIN", { enumerable: true, get: function () { return chains_1.ID_CHAIN; } });
+var compute_1 = require("@zerodao/compute");
+Object.defineProperty(exports, "makeQuoter", { enumerable: true, get: function () { return compute_1.makeQuoter; } });
+Object.defineProperty(exports, "makeCompute", { enumerable: true, get: function () { return compute_1.makeCompute; } });
+const compute_2 = require("@zerodao/compute");
 __exportStar(require("@zerodao/logger"), exports);
-__exportStar(require("@zerodao/p2p"), exports);
-const utils = __importStar(require("@zerodao/utils"));
-exports.utils = utils;
+var p2p_1 = require("@zerodao/p2p");
+Object.defineProperty(exports, "ZeroP2P", { enumerable: true, get: function () { return p2p_1.ZeroP2P; } });
+const utilsModule = __importStar(require("@zerodao/utils"));
 const deployments = require("@zerodao/protocol");
 exports.DEPLOYMENTS = deployments;
+const utils = Object.assign({
+    applyRatio: compute_2.applyRatio,
+    computeRandomValue: compute_2.computeRandomValue,
+    getNonce: compute_2.getNonce,
+    getPNonce: compute_2.getPNonce
+}, utilsModule);
+exports.utils = utils;
 //# sourceMappingURL=index.js.map
