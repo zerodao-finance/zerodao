680d89656d3b24194c80b59408abd23b29eade86
yoyobojo <angellolazar@gmail.com>
2022-09-01 12:24:39 -0400

    finalized

--- packages/webhook/src.ts/middleware.ts
+++ packages/webhook/src.ts/middleware.ts
@@ -2,8 +2,8 @@ import { Buffer } from 'buffer';
 import { recoverAddress } from '@ethersproject/transactions';
 import { hashWebhookMessage } from './webhook';
 
-export const zeroWebhookMiddleware = () => {
-  return (req, res, next, end) => {
+export const zeroWebhookMiddleware = (req, res, next) => {
+  return (req, res, next) => {
     req.signerAddress = recoverAddress(hashWebhookMessage(req.body.serialized), req.body.signature);
     req.deserialized = JSON.parse(Buffer.from(req.body.serialized.substr(2), 'hex').toString('utf8'));
     next();
