a699f525e366f61715afc688be638161f4ce790b
yoyobojo <angellolazar@gmail.com>
2022-09-01 10:31:08 -0400

    added next

--- packages/webhook/src.ts/webhook.ts
+++ packages/webhook/src.ts/webhook.ts
@@ -15,6 +15,7 @@ const zeroWebhookMiddleware = () => {
   return (req, res, next, end) => {
     req.signerAddress = recoverAddress(hashWebhookMessage(req.body.serialized), req.body.signature);
     req.deserialized = JSON.parse(Buffer.from(req.body.serialized.substr(2), 'hex').toString('utf8'));
+    next();
   }
 }
 
