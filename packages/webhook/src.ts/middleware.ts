f549b38d4a476b0d57c2caeef5a3c3c6c4b3baef
altinak <altinak@zero.gg>
2022-09-01 13:17:28 -0500

    works

--- packages/webhook/src.ts/middleware.ts
+++ packages/webhook/src.ts/middleware.ts
@@ -4,7 +4,6 @@ import { hashWebhookMessage } from './webhook';
 
 export const zeroWebhookMiddleware = (req, res, next) => {
   return (req, res, next) => {
-    console.log(req.body);
     req.signerAddress = recoverAddress(hashWebhookMessage(req.body.data), req.body.signature);
     req.deserialized = JSON.parse(Buffer.from(req.body.data.substr(2), 'hex').toString('utf8'));
     next();
