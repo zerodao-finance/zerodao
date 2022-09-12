1405e9e481511b2aa799248a281e2f4c1f6d10d1
d1ll0n <dillonkellar@protonmail.com>
2022-09-11 21:33:57 -0500

    Add network setup fn to Arbitrum test. Deduplicate common tests

--- packages/protocol/contracts/test/BTCVault.Arbitrum.test.sol
+++ packages/protocol/contracts/test/BTCVault.Arbitrum.test.sol
@@ -1,12 +1,25 @@
 pragma solidity >=0.8.13;
 import "./Common.sol";
+import { ConvertWBTCArbitrum } from "../modules/arbitrum/ConvertWBTC.sol";
+import { ConvertUSDCArbitrum } from "../modules/arbitrum/ConvertUSDC.sol";
+import { ConvertNativeArbitrum } from "../modules/arbitrum/ConvertNative.sol";
 
 contract BTCVaultTestArbitrum is Common {
   constructor() {}
 
-  function setUp() public {
-    initiateArbitrumFork();
-    setUpBase();
+  function setUpNetwork() internal virtual override {
+    renbtc = 0xDBf31dF14B66535aF65AaC99C32e9eA844e14501;
+    wbtc = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
+    usdc = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    gateway = 0x05Cadbf3128BcB7f2b89F3dD55E5B0a036a49e20;
+    rencrv = 0x3E01dD8a5E1fb3481F0F589056b428Fc308AF0Fb;
+    btcEthOracle = 0xc5a90A6d7e4Af242dA238FFe279e9f2BA0c64B2e;
+    gasPriceOracle = address(new BlockGasPriceOracle());
+    moduleWBTC = address(new ConvertWBTCArbitrum(renbtc));
+    moduleUSDC = address(new ConvertUSDCArbitrum(renbtc));
+    moduleETH = address(new ConvertNativeArbitrum(renbtc));
+    // @todo arbitrum version
+    renBtcConverter = address(new RenBtcEthConverterMainnet());
   }
 
   function testMockGatewayLogic() public {
@@ -14,30 +27,7 @@ contract BTCVaultTestArbitrum is Common {
   }
 
   function testModuleTypes() public {
-    vault.addModule(moduleDummy, ModuleType.LoanAndRepayOverride, 1000, 1000);
-    vault.removeModule(moduleDummy);
-  }
-
-  function testZeroLoanRenBTC() public {
-    zeroLoan(address(0x0), 1e8);
-  }
-
-  function testZeroLoanWBTC() public {
-    zeroLoan(address(moduleWBTC), 1e8);
-  }
-
-  function testZeroLoanUSDC() public {
-    zeroLoan(address(moduleUSDC), 1e8);
-  }
-
-  function testZeroLoanETH() public {
-    zeroLoan(address(moduleETH), 1e8);
-  }
-
-  function testExpiry() public {
-    bytes memory data;
-    vault.loan(address(0x0), zerowallet, 1000000, 1, data);
-    vm.warp(block.timestamp + 3601);
-    vault.closeExpiredLoan(address(0x0), zerowallet, 1000000, 1, data, address(this));
+    vault.addModule(moduleETH, ModuleType.LoanAndRepayOverride, 1000, 1000);
+    vault.removeModule(moduleETH);
   }
 }
--- packages/protocol/contracts/test/BTCVault.Mainnet.test.sol
+++ packages/protocol/contracts/test/BTCVault.Mainnet.test.sol
@@ -6,92 +6,13 @@ import "./Common.sol";
 contract BTCVaultTestMainnet is Common {
   constructor() {}
 
-  function setUp() public virtual override {
-    initiateMainnetFork();
-    super.setUp();
-  }
-
-  /*//////////////////////////////////////////////////////////////
-                          Deployment Tests
-  //////////////////////////////////////////////////////////////*/
-
-  function testConstructor() external {
-    (
-      address _gatewayRegistry,
-      address _btcEthPriceOracle,
-      address _gasPriceOracle,
-      address _renBtcConverter,
-      uint256 _cacheTimeToLive,
-      uint256 _maxLoanDuration,
-      uint256 _targetEthReserve,
-      uint256 _maxGasProfitShareBips,
-      address _zeroFeeRecipient
-    ) = vault.getConfig();
-    assertEq(_gatewayRegistry, gatewayRegistry);
-    assertEq(_btcEthPriceOracle, btcEthOracle);
-    assertEq(_gasPriceOracle, gasPriceOracle);
-    assertEq(_renBtcConverter, renBtcConverter);
-    assertEq(_cacheTimeToLive, DefaultCacheTTL);
-    assertEq(_maxLoanDuration, DefaultMaxLoanDuration);
-    assertEq(_targetEthReserve, DefaultTargetEthReserve);
-    assertEq(_maxGasProfitShareBips, DefaultMaxGasProfitShareBips);
-    assertEq(_zeroFeeRecipient, address(this));
-    assertEq(vault.name(), "ZeroBTC");
-    assertEq(vault.symbol(), "ZBTC");
-    assertEq(vault.decimals(), 8);
-  }
-
-  function testInitialize() external {
-    (
-      uint256 zeroBorrowFeeBips,
-      uint256 renBorrowFeeBips,
-      uint256 zeroFeeShareBips,
-      uint256 zeroBorrowFeeStatic,
-      uint256 renBorrowFeeStatic,
-      /* uint256 satoshiPerEth */,
-      /* uint256 gweiPerGas */,
-      /* uint256 lastUpdateTimestamp */,
-      /* uint256 totalBitcoinBorrowed */,
-      /* uint256 unburnedGasReserveShares */,
-      /* uint256 unburnedZeroFeeShares */
-    ) = vault.getGlobalState();
-    assertEq(zeroBorrowFeeBips, DefaultZeroBorrowFeeBips);
-    assertEq(renBorrowFeeBips, DefaultRenBorrowFeeBips);
-    assertEq(zeroFeeShareBips, DefaultZeroBorrowFeeStatic);
-    assertEq(zeroBorrowFeeStatic, DefaultRenBorrowFeeStatic);
-    assertEq(renBorrowFeeStatic, DefaultZeroFeeShareBips);
-  }
-
   function testMockGatewayLogic() public {
     mintRenBtc(1);
     assertEq(IERC20(renbtc).balanceOf(address(this)), 1, "RenBTC not being minted");
   }
 
   function testModuleTypes() public {
-    vault.addModule(moduleDummy, ModuleType.LoanAndRepayOverride, 1000, 1000);
-    vault.removeModule(moduleDummy);
-  }
-
-  function testZeroLoanRenBTC() public {
-    zeroLoan(address(0x0), 1e8);
-  }
-
-  function testZeroLoanWBTC() public {
-    zeroLoan(address(moduleWBTC), 1e8);
-  }
-
-  function testZeroLoanUSDC() public {
-    zeroLoan(address(moduleUSDC), 1e8);
-  }
-
-  function testZeroLoanETH() public {
-    zeroLoan(address(moduleETH), 1e8);
-  }
-
-  function testExpiry() public {
-    bytes memory data;
-    vault.loan(address(0x0), zerowallet, 1000000, 1, data);
-    vm.warp(block.timestamp + 3601);
-    vault.closeExpiredLoan(address(0x0), zerowallet, 1000000, 1, data, address(this));
+    vault.addModule(moduleETH, ModuleType.LoanAndRepayOverride, 1000, 1000);
+    vault.removeModule(moduleETH);
   }
 }
