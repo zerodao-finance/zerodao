1827024847000ef758494ceaa7a973b04a99bc21
altinak <altinak@zero.gg>
2022-09-22 10:35:51 -0500

    fix signature parsing

--- packages/webhook/src.ts/middleware.ts
+++ packages/webhook/src.ts/middleware.ts
@@ -3,9 +3,13 @@ import { hexlify } from "@ethersproject/bytes";
 import { fromPlainObject, deserialize } from "@zerodao/request";
 import { hashWebhookMessage } from "./webhook";
 
+const getSignatureHeader = (headers) => {
+  return headers['X-Signature'] || headers['X-signature'] || headers['x-signature'];
+};
 export const zeroWebhookMiddleware = (req, res, next) => {
   return (req, res, next) => {
-    if (!req.headers['X-Signature'] || req.headers['X-Signature'].length !== 132) req.signerAddress = null;
+    const signature = getSignatureHeader(req.headers);
+    if (!signature || typeof signature !== 'string' || signature.length !== 132) req.signerAddress = null;
     else req.signerAddress = recoverAddress(hashWebhookMessage(hexlify(fromPlainObject(req.body).serialize())), req.headers['X-Signature']);
     next();
   }
