8a59a712b33dc3231aa2dab4b312f32ba9c9721f
altinak <altinak@zero.gg>
2022-10-19 09:36:16 -0400

    update

---
+++ packages/p2p/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+module.exports = {
+  entry: './src.ts/index.ts',
+  mode: process.env.NODE_ENV || 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      assert: require.resolve("assert"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify"),
+      fs: false,
+      zlib: require.resolve("browserify-zlib"),
+      url: require.resolve("url"),
+      buffer: require.resolve("buffer"),
+    }
+  },
+  output: {
+    filename: 'p2p.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+};
