ef3e159a2301227ec4c25c30dd502f4db4137994
yoyobojo <angellolazar@gmail.com>
2022-10-25 22:55:46 -0400

    reverted to commit c78d4e1 w/ old libp2p and implemented webpack

--- packages/p2p/webpack.config.js
+++ packages/p2p/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       assert: require.resolve("assert"),
       http: require.resolve("stream-http"),
       https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify"),
+      os: require.resolve("os-browserify/browser"),
       fs: false,
       zlib: require.resolve("browserify-zlib"),
       url: require.resolve("url"),
