f3edbd1782b68b5d953dd04b9445c1fed86c3991
v16s <16210686+v16s@users.noreply.github.com>
2022-12-17 01:45:37 +0530

    fix: everything compiles

--- packages/node/src.ts/rpc/rpc.ts
+++ packages/node/src.ts/rpc/rpc.ts
@@ -1,70 +1,67 @@
-"use strict";
-const grpc = require("@grpc/grpc-js");
-const protoLoader = require("@grpc/proto-loader");
-const EventEmitter = require("events");
-import { type UnaryCallHandler } from "./services";
+import { loadPackageDefinition, Server, ServerCredentials } from "grpc";
+import { loadSync } from "@grpc/proto-loader";
+import { EventEmitter } from "node:events";
+import type { UnaryCallHandler } from "./services";
+import { logger } from "../logger";
 
 export class RPCServer extends EventEmitter {
-	self: any = undefined;
-	path: string = __dirname + "/../../proto/ZeroProtocol.proto";
-	service: any = undefined;
-	pkg: any = undefined;
+  self: any = undefined;
+  path: string = __dirname + "/../../proto/ZeroProtocol.proto";
+  service: any = undefined;
+  pkg: any = undefined;
 
-	static PORT: string = "50051";
+  static PORT: string = "50051";
 
-	static init() {
-		return new RPCServer();
-	}
+  static init() {
+    return new RPCServer();
+  }
 
-	constructor() {
-		super();
-		this.self = new grpc.Server();
-	}
+  constructor() {
+    super();
+    this.self = new Server();
+  }
 
-	start({ port }: any = {}) {
-		this.pkg = grpc.loadPackageDefinition(
-			protoLoader.loadSync(this.path, {
-				keepCase: true,
-				longs: String,
-				enums: String,
-				defualts: true,
-				oneofs: true,
-			})
-		);
+  start({ port }: any = {}) {
+    this.pkg = loadPackageDefinition(
+      loadSync(this.path, {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+      })
+    );
 
-		this.service = (this.pkg as any).RpcService;
+    this.service = (this.pkg as any).RpcService;
 
-		this.self.addService((this.service as any).service, {
-			zero_sendTransaction: this._handleTransaction
-		});
+    this.self.addService((this.service as any).service, {
+      zero_sendTransaction: this._handleTransaction,
+    });
 
-		this.self.bindAsync(
-			`0.0.0.0:${port || RPCServer.PORT}`,
-			grpc.ServerCredentials.createInsecure(),
-			() => {
-				this.self.start();
-			}
-		);
-		return { success: true };
-	}
-	
+    this.self.bindAsync(
+      `0.0.0.0:${port || RPCServer.PORT}`,
+      ServerCredentials.createInsecure(),
+      () => {
+        this.self.start();
+      }
+    );
+    return { success: true };
+  }
 
-	// handle zero_sendTransaction gRPC request
-	_handleTransaction: UnaryCallHandler (call: any, callback: any) {
-		callback(null, () => {
-			try {
-				this._emit("zero_sendTransaction", message);
-				return { status: 0 };
-			} catch (error) {
-				return { status: 1, errorMsg: new TextEncoder().encode(error.message) };
-			}
-		})
-	}
+  // handle zero_sendTransaction gRPC request
+  _handleTransaction: UnaryCallHandler = (message: any, callback: any) => {
+    callback(null, () => {
+      try {
+        this._emit("zero_sendTransaction", message);
+        return { status: 0 };
+      } catch (error) {
+        return { status: 1, errorMsg: new TextEncoder().encode(error.message) };
+      }
+    });
+  };
 
-
-	_emit(eventName, msg) {
-		logger.info(`EMIT: <${event},${msg}>`);
-		super.emit(eventName, msg);
-	}
-
+  _emit(eventName, msg) {
+    logger.info(`EMIT: <${event},${msg}>`);
+    super.emit(eventName, msg);
+  }
 }
