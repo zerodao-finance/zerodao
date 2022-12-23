b26a79578a9c61d6c3d63c599bfd54d92fd2cf02
v16s <16210686+v16s@users.noreply.github.com>
2022-12-23 19:14:22 +0530

    chore: init zero token

---
+++ packages/token/contracts/token/ZERO.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.7 <0.9.0;
+import { ERC20PermitUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract ZERO is OwnableUpgradeable, ERC20PermitUpgradeable {
+  function initialize() public initializer {
+    __ERC20Permit_init("ZERO");
+    __ERC20_init_unchained("ZERO", "ZERO");
+    __Ownable_init_unchained();
+  }
+
+  function mint(address account, uint256 amount) public onlyOwner {
+    _mint(account, amount);
+  }
+
+  function burn(address account, uint256 amount) public onlyOwner {
+    _burn(account, amount);
+  }
+}
