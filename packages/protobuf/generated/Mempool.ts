b873fae0920342887c90489c89a4ebf4d4341cb0
aedsidhe <hughesy102@gmail.com>
2022-12-29 17:56:27 -0500

    add staking

--- packages/protobuf/generated/Mempool.ts
+++ packages/protobuf/generated/Mempool.ts
@@ -1,14 +1,11 @@
 // Original file: proto/ZeroProtocol.proto
 
-import type {
-  Transaction as _Transaction,
-  Transaction__Output as _Transaction__Output,
-} from "./Transaction";
+import type { Transaction as _Transaction, Transaction__Output as _Transaction__Output } from './Transaction';
 
 export interface Mempool {
-  data?: { [key: string]: _Transaction };
+  'data'?: ({[key: string]: _Transaction});
 }
 
 export interface Mempool__Output {
-  data: { [key: string]: _Transaction__Output };
+  'data': ({[key: string]: _Transaction__Output});
 }
