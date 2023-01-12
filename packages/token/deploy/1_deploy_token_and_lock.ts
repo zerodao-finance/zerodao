b7af8aa8567879b433301ee206313cdfc5d39820
v16s <16210686+v16s@users.noreply.github.com>
2023-01-12 16:13:49 +0530

    chore: init masterchef.sol

--- packages/token/deploy/1_deploy_token_and_lock.ts
+++ packages/token/deploy/1_deploy_token_and_lock.ts
@@ -1,6 +1,6 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import type { HardhatEthersHelpers } from "hardhat-deploy-ethers/types";
-import { ZERO } from "../typechain-types";
+import { ZERO, SZERO } from "../typechain-types";
 import { ethers as _ethers } from "ethers";
 
 const deploy: DeployFunction = async (hre) => {
@@ -38,6 +38,38 @@ const deploy: DeployFunction = async (hre) => {
       },
     },
   });
+  const deployedsZero = await deployments.deploy("sZERO", {
+    from: signer.address,
+    proxy: defaultProxyOptions,
+  });
+  const sZero = new ethers.Contract(
+    deployedsZero.address,
+    deployedsZero.abi,
+    signer
+  ) as SZERO;
+  const deployedMasterChef = await deployments.deploy("MasterChef", {
+    from: signer.address,
+    proxy: {
+      owner: signer.address,
+      proxyContract: "OpenZeppelinTransparentProxy",
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [
+            deployedsZero.address,
+            signer.address,
+            /*zero per block*/
+            2000,
+            /*start block*/
+            await hre.network.provider.send("eth_getBlockNumber", []),
+            /*end block*/
+            0,
+          ],
+        },
+      },
+    },
+  });
+  await sZero.setMasterChef(deployedMasterChef.address);
   await deployments.save("ZeroLock", deployedZeroLock);
 
   await zero.mint(signer.address, ethers.utils.parseEther("1000"));
--- packages/token/typechain-types/@openzeppelin/contracts-upgradeable/utils/index.ts
+++ packages/token/typechain-types/@openzeppelin/contracts-upgradeable/utils/index.ts
@@ -3,4 +3,8 @@
 /* eslint-disable */
 import type * as cryptography from "./cryptography";
 export type { cryptography };
+import type * as introspection from "./introspection";
+export type { introspection };
+import type * as structs from "./structs";
+export type { structs };
 export type { ContextUpgradeable } from "./ContextUpgradeable";
