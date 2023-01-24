98c04319a75eadf1d131a8ce11131c24e523e98f
yoyobojo <angellolazar@gmail.com>
2023-01-24 00:49:01 -0500

    distributing for stake fe test

--- packages/token/deploy/2_deploy_and_distribute.ts
+++ packages/token/deploy/2_deploy_and_distribute.ts
@@ -4,13 +4,13 @@ import { ZERO, SZERO } from "../typechain-types";
 import { ethers as _ethers } from "ethers";
 
 const deploy: DeployFunction = async (hre) => {
-  if(process.env.TEST) return;
-
+  if(!process.env.TEST) return;
+  
   //@ts-ignore
   const ethers: typeof _ethers & HardhatEthersHelpers = hre.ethers;
   //@ts-ignore
   const deployments = hre.deployments;
-  const [signer] = await ethers.getSigners();
+  const [signer, signer2] = await ethers.getSigners();
   const defaultProxyOptions = {
     owner: signer.address,
     methodName: "initialize",
@@ -62,7 +62,6 @@ const deploy: DeployFunction = async (hre) => {
       },
     },
   });
-  console.log(Number(await hre.network.provider.send("eth_blockNumber", [])));
   const deployedSZero = await deployments.deploy("sZERO", {
     from: signer.address,
     proxy: {
@@ -92,6 +91,19 @@ const deploy: DeployFunction = async (hre) => {
   await zero.mint(signer.address, ethers.utils.parseEther("100000"));
   await zero.approve(deployedSZero.address, ethers.utils.parseEther("1000000"));
   await zero.transferOwnership(deployedSZero.address);
+
+  console.log("Treasury ZERO Balance:", await zero.balanceOf(deployedSZero.address))
+  console.log("Signer ZERO Balance:", await zero.balanceOf(signer.address))
+
+  // Distribution For Stake Test
+  console.log(`\n\nZERO: ${zero.address} | ${await zero.decimals()} | ${await zero.name()}`);
+    const wallet = new _ethers.Wallet(process.env.WALLET || '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'); // Defaults to 'Account #1'
+    console.log(`\n\nBEFORE --- Wallet (${wallet.address}) Balance:`, (await zero.balanceOf(wallet.address)).toString())
+    const tx = await zero.approve(wallet.address, ethers.utils.parseEther("1000000"));
+    console.log("\n\nApproving TX:", tx.hash);
+    const tx2 = await zero.connect(signer).transfer(wallet.address, ethers.utils.parseEther("10000"))
+    console.log(`\n\nSending ZERO to ${wallet.address} TX:`, tx2.hash);
+    console.log(`\n\nAFTER --- Wallet (${wallet.address}) Balance:`, (await zero.balanceOf(wallet.address)).toString())
 };
 
 export default deploy;
