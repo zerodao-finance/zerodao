d3bc6a81c91d508778f5b70f1c5c14a237d469c5
yoyobojo <angellolazar@gmail.com>
2023-01-25 17:30:55 -0500

    added merkle tree creation

--- packages/token/deploy/2_deploy_and_distribute.ts
+++ packages/token/deploy/2_deploy_and_distribute.ts
@@ -2,6 +2,9 @@ import { DeployFunction } from "hardhat-deploy/types";
 import type { HardhatEthersHelpers } from "hardhat-deploy-ethers/types";
 import { ZERO } from "../typechain-types";
 import { ethers as _ethers } from "ethers";
+import path from "path";
+import fs from 'fs';
+import { useMerkleGenerator } from "../merkle/use-merkle";
 
 const deploy: DeployFunction = async (hre) => {
   if(!process.env.TEST) return;
@@ -33,15 +36,22 @@ const deploy: DeployFunction = async (hre) => {
     signer
   ) as ZERO;
 
+  // Merkle Tree Creation
+  console.log('\n\n---- Merkle Tree Started ----');
+  const merkleDir = path.join(__dirname, '..', 'merkle', process.env.TEST ? 'localhost' : 'mainnet');
+  const merkleInput = require(path.join(merkleDir, 'input'));
+  const merkleTree = useMerkleGenerator(merkleInput);
+  console.log(merkleTree);
+  await fs.writeFileSync(path.join(merkleDir, 'airdrop.json'), JSON.stringify(merkleTree, null, 2));
+  console.log('---- Merkle Tree Created ----');
+
   // Distribution For Stake Test
-  console.log(`\n\nZERO: ${zero.address} | ${await zero.decimals()} | ${await zero.name()}`);
-    const wallet = new _ethers.Wallet(process.env.WALLET || '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'); // Defaults to 'Account #1'
-    console.log(`\n\nBEFORE --- Wallet (${wallet.address}) Balance:`, (await zero.balanceOf(wallet.address)).toString())
-    const tx = await zero.approve(wallet.address, ethers.utils.parseEther("1000000"));
-    console.log("\n\nApproving TX:", tx.hash);
-    const tx2 = await zero.connect(signer).transfer(wallet.address, ethers.utils.parseEther("10000"))
-    console.log(`\n\nSending ZERO to ${wallet.address} TX:`, tx2.hash);
-    console.log(`\n\nAFTER --- Wallet (${wallet.address}) Balance:`, (await zero.balanceOf(wallet.address)).toString())
+  console.log('\n\n---- ZERO Distribution Started ----');
+  const wallet = new _ethers.Wallet(process.env.WALLET || '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'); // Defaults to 'Account #1'
+  const tx = await zero.approve(wallet.address, ethers.utils.parseEther("1000000"));
+  const tx2 = await zero.connect(signer).transfer(wallet.address, ethers.utils.parseEther("10000"))
+  console.log(`Sending ZERO to ${wallet.address} TX:`, tx2.hash);
+  console.log('---- ZERO Distribution Completed ----\n\n');
 };
 
 export default deploy;
