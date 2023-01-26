7431e398add9533e64b441eab90471f0d5160ce0
yoyobojo <angellolazar@gmail.com>
2023-01-26 10:13:43 -0500

    merkle implemented

--- packages/token/deploy/2_deploy_and_distribute.ts
+++ packages/token/deploy/2_deploy_and_distribute.ts
@@ -1,6 +1,6 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import type { HardhatEthersHelpers } from "hardhat-deploy-ethers/types";
-import { ZERO } from "../typechain-types";
+import { SZERO, ZERO } from "../typechain-types";
 import { ethers as _ethers } from "ethers";
 import path from "path";
 import fs from 'fs';
@@ -28,15 +28,55 @@ const deploy: DeployFunction = async (hre) => {
       },
     },
   });
+  const deployedZeroFrost = await deployments.deploy("ZEROFROST", {
+    from: signer.address,
+    proxy: {
+      owner: signer.address,
+      proxyContract: "OpenZeppelinTransparentProxy",
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [],
+        },
+      },
+    },
+  });
+  const deployedSZero = await deployments.deploy("sZERO", {
+    from: signer.address,
+    proxy: {
+      owner: signer.address,
+      proxyContract: "OpenZeppelinTransparentProxy",
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [
+            deployedZero.address,
+            /*zerofrost*/
+            deployedZeroFrost.address,
+            /*devaddr*/
+            signer.address,
+            /*zero per block*/
+            ethers.utils.parseEther("2000"),
+            /*bonus end block*/
+            await hre.network.provider.send("eth_blockNumber", []),
+          ],
+        },
+      },
+    },
+  });
 
-  await deployments.save("ZERO", deployedZero);
   const zero = new ethers.Contract(
     deployedZero.address,
     deployedZero.abi,
     signer
   ) as ZERO;
+  const sZero = new ethers.Contract(
+    deployedSZero.address,
+    deployedSZero.abi,
+    signer
+  ) as SZERO
 
-  // Merkle Tree Creation
+  // Merkle tree creation
   console.log('\n\n---- Merkle Tree Started ----');
   const merkleDir = path.join(__dirname, '..', 'merkle', process.env.TEST ? 'localhost' : 'mainnet');
   const merkleInput = require(path.join(merkleDir, 'input'));
@@ -45,7 +85,7 @@ const deploy: DeployFunction = async (hre) => {
   await fs.writeFileSync(path.join(merkleDir, 'airdrop.json'), JSON.stringify(merkleTree, null, 2));
   console.log('---- Merkle Tree Created ----');
 
-  // Distribution For Stake Test
+  // Distribution for staking tests
   console.log('\n\n---- ZERO Distribution Started ----');
   const wallet = new _ethers.Wallet(process.env.WALLET || '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'); // Defaults to 'Account #1'
   const tx = await zero.approve(wallet.address, ethers.utils.parseEther("1000000"));
